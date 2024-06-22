d5b05c5802dbdc706c45b11d09bc5708e2b383cf
Gutemberg Lima <j.berglima05@gmail.com>
2024-06-22 12:18:48 -0300

    feat: implement crud noticeController

--- backend/src/controller/notice/noticeController.js
+++ backend/src/controller/notice/noticeController.js
@@ -4,9 +4,30 @@ import { formatDates } from "../../service/noticeService.js";
 
 export const register = async (req, res) => {
     const { body } = req
+    const dataExpiracao = new Date(body.data_expiracao);
+    try {
+        const [ noticeInfo ] = await knex('aviso').insert({...body, data_expiracao: dataExpiracao}).returning('*')
+
+        const formattedDates = formatDates(noticeInfo.data_criacao,noticeInfo.data_atualizacao,noticeInfo.data_expiracao,3);
+        
+        noticeInfo.data_criacao = formattedDates.data_criacao;
+        noticeInfo.data_atualizacao = formattedDates.data_atualizacao;
+        noticeInfo.data_expiracao = formattedDates.data_expiracao;
+
+        msgJson(201, res, noticeInfo, true)
+    } catch (error) {
+        msgJson(500, res, 'Erro interno do servidor ao cadastrar aviso.', false)
+    }
+}
+
+export const update = async (req, res) => {
+    const { params: { id: id_aviso }, body} = req
 
     try {
-        const [ noticeInfo ] = await knex('aviso').insert({...body}).returning('*')
+        const idInfo = await knex('aviso').where({ id_aviso }).returning('*');
+        if (!idInfo || idInfo.length === 0 ) return msgJson(404, res, 'Aviso não encontrado.')
+
+        const [ noticeInfo ] = await knex('aviso').update({...body}).where({ id_aviso }).returning('*')
 
         const formattedDates = formatDates(noticeInfo.data_criacao,noticeInfo.data_atualizacao,noticeInfo.data_expiracao,3);
         
@@ -17,6 +38,70 @@ export const register = async (req, res) => {
         msgJson(201, res, noticeInfo, true)
     } catch (error) {
         console.error(error)
-        msgJson(500, res, 'Erro interno do servidor ao cadastrar aviso.', false)
+        msgJson(500, res, 'Erro interno do servidor ao cadastrar academia.', false)
+    }
+}
+
+export const deleteNotice = async(req, res) => {
+    const { params: { id: id_aviso }} = req
+
+    try {
+        let noticeInfo = await knex('aviso').where({ id_aviso }).first().returning('*');
+        if (!noticeInfo || noticeInfo.length === 0 ) return msgJson(404, res, 'Aviso não encontrado.')
+
+        const formattedDates = formatDates(noticeInfo.data_criacao,noticeInfo.data_atualizacao,noticeInfo.data_expiracao,3);
+        
+        noticeInfo.data_criacao = formattedDates.data_criacao;
+        noticeInfo.data_atualizacao = formattedDates.data_atualizacao;
+        noticeInfo.data_expiracao = formattedDates.data_expiracao;
+
+        await knex('aviso').where({ id_aviso }).del().returning('*');
+
+        msgJson(201, res, noticeInfo, true)
+    } catch (error) {
+        console.error(error)
+        msgJson(500, res, 'Erro interno do servidor ao detalhar avisos.', false)
+    }
+}
+
+
+export const getNoticeById = async(req, res) => {
+    const { params: { id: id_aviso }} = req
+
+    try {
+        const noticeInfo = await knex('aviso').where({ id_aviso }).first().returning('*');
+        if (!noticeInfo || noticeInfo.length === 0 ) return msgJson(404, res, 'Aviso não encontrado.')
+
+        const formattedDates = formatDates(noticeInfo.data_criacao,noticeInfo.data_atualizacao,noticeInfo.data_expiracao,3);
+        
+        noticeInfo.data_criacao = formattedDates.data_criacao;
+        noticeInfo.data_atualizacao = formattedDates.data_atualizacao;
+        noticeInfo.data_expiracao = formattedDates.data_expiracao;
+
+        msgJson(201, res, noticeInfo, true)
+    } catch (error) {
+        console.error(error)
+        msgJson(500, res, 'Erro interno do servidor ao detalhar aviso.', false)
+    }
+}
+
+export const getAllNotice = async(req, res) => {
+    try {
+        const noticeInfo = await knex('aviso').select('*');
+        const formattedNoticeInfo = noticeInfo.map(notice => {
+            const formattedDates = formatDates(notice.data_criacao,notice.data_atualizacao,notice.data_expiracao,3);
+
+            return {
+                ...notice,
+                data_criacao: formattedDates.data_criacao,
+                data_atualizacao: formattedDates.data_atualizacao,
+                data_expiracao: formattedDates.data_expiracao
+            };
+        });
+
+        msgJson(201, res, formattedNoticeInfo, true)
+    } catch (error) {
+        console.error(error)
+        msgJson(500, res, 'Erro interno do servidor ao detalhar avisos.', false)
     }
 }
