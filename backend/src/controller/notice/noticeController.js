790404f4e7eb12c0131d6ef7e261dd6c7575b4d3
Gutemberg Lima <j.berglima05@gmail.com>
2024-06-22 10:30:57 -0300

    feat: implement route notice

--- backend/src/controller/notice/noticeController.js
+++ backend/src/controller/notice/noticeController.js
@@ -1,17 +1,25 @@
 import { msgJson } from "../../utils/responseJson.js"
 import { knex } from "../../database/connection/dbConnection.js"
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale'
+import { formatInTimeZone } from 'date-fns-tz'
 
 export const register = async (req, res) => {
-    const { body, dataUnique} = req
+    const { body } = req
 
     try {
-        if (dataUnique && dataUnique.field) return msgJson(400, res, `O campo '${dataUnique.field}' com valor '${dataUnique.idObj.nome}' já está em uso.`, false);
+        const [ noticeInfo ] = await knex('aviso').insert({...body}).returning('*')
 
-            const [ noticeInfo ] = await knex('aviso').insert({...body}).returning('*')
+        const dataAtualizacao = new Date(noticeInfo.data_atualizacao);
+        dataAtualizacao.setHours(dataAtualizacao.getHours() - 3);
+
+        noticeInfo.data_criacao = formatInTimeZone(new Date(noticeInfo.data_criacao), 'America/Sao_Paulo', 'dd/MM/yyyy HH:mm:ss', { locale: ptBR } )
+        noticeInfo.data_atualizacao = formatInTimeZone(dataAtualizacao, 'America/Sao_Paulo', 'dd/MM/yyyy HH:mm:ss', { locale: ptBR });
+        noticeInfo.data_expiracao = format(new Date(noticeInfo.data_expiracao), 'dd/MM/yyyy HH:mm:ss', { locale: ptBR });
 
         msgJson(201, res, noticeInfo, true)
     } catch (error) {
         console.error(error)
-        msgJson(500, res, 'Erro interno do servidor ao cadastrar academia.', false)
+        msgJson(500, res, 'Erro interno do servidor ao cadastrar aviso.', false)
     }
 }
