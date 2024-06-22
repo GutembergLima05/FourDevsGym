09a5093ef7ed5976cc06479b3a20b4d7bf38fe24
Gutemberg Lima <j.berglima05@gmail.com>
2024-06-22 15:55:36 -0300

    feat: added cors implementation

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import { allRoutes } from './routes/exportRoute.js';
 import dotenv from 'dotenv'
+import cors from 'cors';
 
 dotenv.config()
 
 const app = express(), port = process.env.PORT || 3000
 
+
+app.use(cors());
 app.use(express.json(), allRoutes);
 
 app.listen(port, () => console.log(`SERVIDOR ON! NA PORTA ${port} `))
