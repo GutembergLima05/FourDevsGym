9fe48d449439bcdbc2d0ba25b62b43a356948273
Gutemberg Lima <j.berglima05@gmail.com>
2024-06-22 12:18:48 -0300

    feat: implement crud noticeController

--- backend/src/routes/noticeRoutes/noticeRouter.js
+++ backend/src/routes/noticeRoutes/noticeRouter.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
 import { validateEntry } from "../../middleware/middlewares.js";
-import { s_notice,  } from "../../schemas/schema.js";
+import { s_notice, s_idCheck  } from "../../schemas/schema.js";
 import * as notice from "../../controller/notice/noticeController.js"
 
 export const routeNotice = Router();
 
-routeNotice.route('/notice').post(validateEntry(s_notice, 'body'), notice.register)
+routeNotice.route('/notice')
+.post(validateEntry(s_notice, 'body'), notice.register)
+.get(notice.getAllNotice)
+
+routeNotice.route('/notice/:id')
+.all(validateEntry(s_idCheck, 'params'))
+.get(notice.getNoticeById)
+.put(validateEntry(s_notice, 'body'), notice.update)
+.delete(notice.deleteNotice)
