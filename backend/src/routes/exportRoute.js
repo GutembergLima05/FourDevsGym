326a271215fdeef8e5cf878f4004161eeae32691
Gutemberg Lima <j.berglima05@gmail.com>
2024-06-22 12:49:35 -0300

    feat: exercise implement crud

--- backend/src/routes/exportRoute.js
+++ backend/src/routes/exportRoute.js
@@ -1,9 +1,11 @@
 import { routeAdm } from "./admRoutes/admRouter.js";
+import { routeExercise } from "./exerciseRoutes/exerciseRouter.js";
 import { routeGym } from "./gymRoutes/gymRouter.js";
 import { routeNotice } from "./noticeRoutes/noticeRouter.js";
 
 export const allRoutes = [
     routeAdm,
     routeGym,
-    routeNotice
+    routeNotice,
+    routeExercise
 ];
--- backend/src/schemas/schema.js
+++ backend/src/schemas/schema.js
@@ -4,6 +4,7 @@ const email = Joi.string().email().max(30).required(),
     senha = Joi.string().min(8).max(72).required(),
     cargo = Joi.string().valid('recepcionista', 'personal', 'dono').required(),
     descricao = Joi.string().max(100).required(),
+    gif_url = Joi.string().max(100).required(),
     nome = Joi.string().max(50).required(),
     endereco = Joi.string().max(50).required(),
     data_expiracao = Joi.string().isoDate().required(),
@@ -13,6 +14,7 @@ const email = Joi.string().email().max(30).required(),
     bairro = Joi.string(),
     cidade = Joi.string(),
     estado = Joi.string(),
+    id_administrador = Joi.number().integer().positive().required(),
     id = Joi.number().integer().positive().required(),
     idOpc = Joi.number().integer().positive(),
     id_academia = Joi.number().integer().positive().required(),
@@ -26,6 +28,8 @@ export const s_idOpcional = criaObjJoi({ categoria_id: idOpc })
 export const s_notice = criaObjJoi({ titulo, descricao, data_expiracao, id_academia })
 
 export const s_admSchema = criaObjJoi({ nome, email, senha, cargo, id_academia })
+
+export const s_exercise = criaObjJoi({ nome, descricao, id_administrador, gif_url })
 
 export const s_login = criaObjJoi({ email, senha })
 
