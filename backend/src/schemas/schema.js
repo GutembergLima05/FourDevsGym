5d4ac2ed174086e6f1d8184f0fde453ecda1b9d8
Gutemberg Lima <j.berglima05@gmail.com>
2024-06-22 10:30:57 -0300

    feat: implement route notice

--- backend/src/schemas/schema.js
+++ backend/src/schemas/schema.js
@@ -3,11 +3,11 @@ import Joi from 'joi'
 const email = Joi.string().email().max(30).required(),
     senha = Joi.string().min(8).max(72).required(),
     cargo = Joi.string().valid('recepcionista', 'personal', 'dono').required(),
-    academia = Joi.string().max(20).required(),
+    descricao = Joi.string().max(100).required(),
     nome = Joi.string().max(50).required(),
     endereco = Joi.string().max(50).required(),
-    cpf = Joi.string().required(),
-    cep = Joi.string(),
+    data_expiracao = Joi.string().isoDate().required(),
+    titulo = Joi.string().max(100).required(),
     rua = Joi.string(),
     numero = Joi.string(),
     bairro = Joi.string(),
@@ -22,6 +22,8 @@ const email = Joi.string().email().max(30).required(),
 export const s_idCheck = criaObjJoi({id})
 
 export const s_idOpcional = criaObjJoi({ categoria_id: idOpc })
+
+export const s_notice = criaObjJoi({ titulo, descricao, data_expiracao, id_academia })
 
 export const s_admSchema = criaObjJoi({ nome, email, senha, cargo, id_academia })
 
