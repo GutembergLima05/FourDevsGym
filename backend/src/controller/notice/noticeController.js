02393419391bdc47b4867991500440852e649227
Gutemberg Lima <j.berglima05@gmail.com>
2024-06-22 11:06:15 -0300

    refact: update route notice

--- backend/src/controller/notice/noticeController.js
+++ backend/src/controller/notice/noticeController.js
@@ -1,8 +1,6 @@
 import { msgJson } from "../../utils/responseJson.js"
-import { knex } from "../../database/connection/dbConnection.js"
-import { format } from 'date-fns';
-import { ptBR } from 'date-fns/locale'
-import { formatInTimeZone } from 'date-fns-tz'
+import { knex } from "../../database/connection/dbConnection.js";
+import { formatDates } from "../../service/noticeService.js";
 
 export const register = async (req, res) => {
     const { body } = req
@@ -10,12 +8,11 @@ export const register = async (req, res) => {
     try {
         const [ noticeInfo ] = await knex('aviso').insert({...body}).returning('*')
 
-        const dataAtualizacao = new Date(noticeInfo.data_atualizacao);
-        dataAtualizacao.setHours(dataAtualizacao.getHours() - 3);
-
-        noticeInfo.data_criacao = formatInTimeZone(new Date(noticeInfo.data_criacao), 'America/Sao_Paulo', 'dd/MM/yyyy HH:mm:ss', { locale: ptBR } )
-        noticeInfo.data_atualizacao = formatInTimeZone(dataAtualizacao, 'America/Sao_Paulo', 'dd/MM/yyyy HH:mm:ss', { locale: ptBR });
-        noticeInfo.data_expiracao = format(new Date(noticeInfo.data_expiracao), 'dd/MM/yyyy HH:mm:ss', { locale: ptBR });
+        const formattedDates = formatDates(noticeInfo.data_criacao,noticeInfo.data_atualizacao,noticeInfo.data_expiracao,3);
+        
+        noticeInfo.data_criacao = formattedDates.data_criacao;
+        noticeInfo.data_atualizacao = formattedDates.data_atualizacao;
+        noticeInfo.data_expiracao = formattedDates.data_expiracao;
 
         msgJson(201, res, noticeInfo, true)
     } catch (error) {
