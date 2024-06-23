a587cf94ad369c4c492c0f402281c016fd571ad6
Gutemberg Lima <j.berglima05@gmail.com>
2024-06-23 15:08:33 -0300

    feat: implementing require token

--- backend/src/routes/exerciseRoutes/exerciseRouter.js
+++ backend/src/routes/exerciseRoutes/exerciseRouter.js
@@ -6,11 +6,13 @@ import * as exercise from "../../controller/exercise/exerciseController.js"
 export const routeExercise = Router();
 
 routeExercise.route('/exercise')
+.all(validateTokenAndRole('administrador','id_adm'))
 .post(validateEntry(s_exercise, 'body'), exercise.register)
 .get(exercise.getAllExercise)
 
+
 routeExercise.route('/exercise/:id')
-.all(validateEntry(s_idCheck, 'params'))
+.all(validateEntry(s_idCheck, 'params'), validateTokenAndRole('administrador','id_adm'))
 .put(validateEntry(s_exercise, 'body'), exercise.update)
 .get(exercise.getExerciseById)
 .delete(exercise.deleteExercise)
