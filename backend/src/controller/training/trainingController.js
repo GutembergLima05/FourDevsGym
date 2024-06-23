9f1da22c17b3b77e167897335021f7fc6d471825
Gutemberg Lima <j.berglima05@gmail.com>
2024-06-23 14:41:20 -0300

    fix: refact trainingController update

--- backend/src/controller/training/trainingController.js
+++ backend/src/controller/training/trainingController.js
@@ -3,123 +3,134 @@ import { knex } from "../../database/connection/dbConnection.js"
 import { formatDates } from "../../service/noticeService.js";
 
 export const register = async (req, res) => {
-    const { body } = req
-    const { nome, descricao, id_administrador, id_dia, id_exercicios } = body;
-    const id_adm  = id_administrador
+    const { body } = req;
+    const { nome, descricao, id_administrador, dias } = body;
+  
     try {
+      // Verificação do administrador
+      const [idAdministrador] = await knex('administrador').where({ id_adm: id_administrador }).returning('*');
+      if (!idAdministrador) return msgJson(404, res, 'Administrador não encontrado.');
+   
+    //Verificação de exercicios
+    // Obtém todos os IDs de exercícios do corpo da requisição
+    const id_exercicios = dias.flatMap(dia => dia.exercicios.map(exercicio => exercicio.id_exercicio))
 
-        const [idAdministrador, idDia] = await Promise.all([
-            knex('administrador').where({ id_adm }).returning('*'),
-            knex('dia').where({ id_dia }).returning('*').first(),
-        ]);
-
-        const exerciciosPromises = id_exercicios.map(id_exercicio => knex('exercicio').where({ id_exercicio }).first());
-        const exercicios = await Promise.all(exerciciosPromises);
-        if (exercicios.some(exercicio => !exercicio)) {
-            const idsExerciciosNaoEncontrados = id_exercicios.filter(id => !exercicios.find(ex => ex && ex.id_exercicio === id));
-            return msgJson(404, res, `Exercício(s) não encontrado(s): ${idsExerciciosNaoEncontrados.join(', ')}`);
-        }
-
-        if (!idAdministrador || idAdministrador.length === 0 ) return msgJson(404, res, 'Administrador não encontrado.')
-        if (!idDia || idDia.length === 0 ) return msgJson(404, res, 'Dia não encontrado.')
-
-        const [ trainingInfo ] = await knex('treino').insert({nome, descricao, id_administrador}).returning('*')
-
-        const formattedDates = formatDates(trainingInfo.data_criacao,trainingInfo.data_atualizacao,null,3);
-        
-        trainingInfo.data_criacao = formattedDates.data_criacao;
-        trainingInfo.data_atualizacao = formattedDates.data_atualizacao;
-
-        await knex('treino_dia').insert({
-            id_treino: trainingInfo.id_treino,
-            id_dia: id_dia
-        });
-
-        for (const id_exercicio of id_exercicios) {
-            await knex('exercicio_treino').insert({
-                id_treino: trainingInfo.id_treino,
-                id_exercicio: id_exercicio
-            });
-        }
-
-    const exerciciosList = await knex('exercicio')
-    .join('exercicio_treino', 'exercicio.id_exercicio', 'exercicio_treino.id_exercicio')
-    .where('exercicio_treino.id_treino', trainingInfo.id_treino)
-    .select('exercicio.*');
-
-
-        const response = { treino: trainingInfo, dia: idDia, exercicios: exerciciosList };
-
-        msgJson(201, res, response, true)
-    } catch (error) {
-        console.error(error)
-        msgJson(500, res, 'Erro interno do servidor ao cadastrar treino.', false)
-    }
+    // Verifica se todos os IDs de exercícios existem na tabela 'exercicio'
+    const exerciciosPromises = id_exercicios.map(id_exercicio => knex('exercicio').where('id_exercicio', id_exercicio).first());
+    const exercicios = await Promise.all(exerciciosPromises);
+if (exercicios.some(exercicio => !exercicio)) {
+    const idsExerciciosNaoEncontrados = id_exercicios.filter(id => !exercicios.find(ex => ex && ex.id_exercicio === id));
+    return msgJson(404, res, `Exercício(s) não encontrado(s): ${idsExerciciosNaoEncontrados.join(', ')}`);
 }
+  
+      // Criação do treino
+      const [trainingInfo] = await knex('treino').insert({ nome, descricao, id_administrador }).returning('*');
+  
+      // Formatação das datas do treino
+      const formattedDates = formatDates(trainingInfo.data_criacao, trainingInfo.data_atualizacao, null, 3);
+      trainingInfo.data_criacao = formattedDates.data_criacao;
+      trainingInfo.data_atualizacao = formattedDates.data_atualizacao;
+  
+      // Inserção dos dias, exercícios, repetições e séries associados
+      for (const dia of dias) {
+        const { id_dia, exercicios } = dia;
+  
+        // Inserir o vínculo na tabela treino_dia_exercicio para cada dia e exercício com repetições e séries
+        for (const exercicio of exercicios) {
+          const { id_exercicio, repeticoes, series } = exercicio;
+  
+          await knex('treino_dia_exercicio').insert({
+            id_treino: trainingInfo.id_treino,
+            id_dia,
+            id_exercicio,
+            repeticoes,
+            series
+          });
+        }
+      }
+      const response = { treino: trainingInfo, dias };
+  
+      msgJson(201, res, response, true); 
+    } catch (error) {
+      console.error(error);
+      msgJson(500, res, 'Erro interno do servidor ao cadastrar treino.', false); 
+    }
+};
 
 export const update = async(req, res) => {
-    const { params: { id: id_treino }, body } = req
-    const { nome, descricao, id_administrador, id_dia, id_exercicios } = body;
-    const id_adm  = id_administrador
+    const { params: { id: id_treino }, body } = req;
+    const { nome, descricao, id_administrador, dias } = body;
 
     try {
-        const [idAdministrador, idTreinoInfo, idDia, treinoDiaExists] = await Promise.all([
-            knex('administrador').where({ id_adm }).returning('*'),
-            knex('treino').where({ id_treino }).returning('*').first(),
-            knex('dia').where({ id_dia }).returning('*').first(),
-            knex('treino_dia').where({ id_treino, id_dia }).first()
-        ]);
+        // Verificação de administrador
+        const administrador = await knex('administrador').where({ id_adm: id_administrador }).first();
+        if (!administrador) return msgJson(404, res, 'Administrador não encontrado.');
 
-        for (let id_exercicio of id_exercicios) {
-            const exercicioTreinoExists = await knex('exercicio_treino').where({ id_treino, id_exercicio }).first();
-            if (!exercicioTreinoExists) {
-                return msgJson(400, res, `O exercício ${id_exercicio} e o treino ${id_treino} não está vinculado ao treino na tabela exercicio_treino.`);
+        // Verificação de treino
+        const treino = await knex('treino').where({ id_treino }).first();
+        if (!treino) return msgJson(404, res, 'Treino não encontrado.');
+
+         //Verificação de exercicios
+    // Obtém todos os IDs de exercícios do corpo da requisição
+    const id_exercicios = dias.flatMap(dia => dia.exercicios.map(exercicio => exercicio.id_exercicio))
+
+    // Verifica se todos os IDs de exercícios existem na tabela 'exercicio'
+    const exerciciosPromises = id_exercicios.map(id_exercicio => knex('exercicio').where('id_exercicio', id_exercicio).first());
+    const exercicios = await Promise.all(exerciciosPromises);
+if (exercicios.some(exercicio => !exercicio)) {
+    const idsExerciciosNaoEncontrados = id_exercicios.filter(id => !exercicios.find(ex => ex && ex.id_exercicio === id));
+    return msgJson(404, res, `Exercício(s) não encontrado(s): ${idsExerciciosNaoEncontrados.join(', ')}`);
+}
+
+        // Atualização do treino
+        await knex('treino').where({ id_treino }).update({ nome, descricao, id_administrador });
+
+        // Remoção dos relacionamentos antigos
+        await knex('treino_dia_exercicio').where({ id_treino }).del();
+
+        // Inserção dos novos relacionamentos
+        for (const dia of dias) {
+            for (const exercicio of dia.exercicios) {
+                await knex('treino_dia_exercicio').insert({
+                    id_treino,
+                    id_dia: dia.id_dia,
+                    id_exercicio: exercicio.id_exercicio,
+                    repeticoes: exercicio.repeticoes,
+                    series: exercicio.series
+                });
             }
         }
 
-        const exerciciosPromises = id_exercicios.map(id_exercicio => knex('exercicio').where({ id_exercicio }).first());
-        const exercicios = await Promise.all(exerciciosPromises);
-        if (exercicios.some(exercicio => !exercicio)) {
-            const idsExerciciosNaoEncontrados = id_exercicios.filter(id => !exercicios.find(ex => ex && ex.id_exercicio === id));
-            return msgJson(404, res, `Exercício(s) não encontrado(s): ${idsExerciciosNaoEncontrados.join(', ')}`);
-        }
-        if (!treinoDiaExists || treinoDiaExists.length === 0) return msgJson(400, res, 'Não há relação válida na tabela treino_dia para este treino e dia.');    
-        if (!idAdministrador || idAdministrador.length === 0 ) return msgJson(404, res, 'Administrador não encontrado.')
-        if (!idTreinoInfo || idTreinoInfo.length === 0 ) return msgJson(404, res, 'Treino não encontrado.')
-        if (!idDia || idDia.length === 0 ) return msgJson(404, res, 'Dia não encontrado.')
+        // Formatação das datas
+        const formattedDates = formatDates(treino.data_criacao, treino.data_atualizacao, null, 3);
+        treino.data_criacao = formattedDates.data_criacao;
+        treino.data_atualizacao = formattedDates.data_atualizacao;
 
-        const formattedDates = formatDates(idTreinoInfo.data_criacao, idTreinoInfo.data_atualizacao, null, 3);
-        idTreinoInfo.data_criacao = formattedDates.data_criacao;
-        idTreinoInfo.data_atualizacao = formattedDates.data_atualizacao
+        // Construção da resposta
+        const diasExercicios = await knex('treino_dia_exercicio')
+            .join('dia', 'treino_dia_exercicio.id_dia', 'dia.id_dia')
+            .join('exercicio', 'treino_dia_exercicio.id_exercicio', 'exercicio.id_exercicio')
+            .where('treino_dia_exercicio.id_treino', id_treino)
+            .select('dia.id_dia', 'dia.nome as dia_nome', 'exercicio.id_exercicio', 'exercicio.nome as exercicio_nome', 'treino_dia_exercicio.repeticoes', 'treino_dia_exercicio.series');
 
-        await knex('treino').where(idTreinoInfo.id_treino).update({nome, descricao, id_administrador})
+        const diasMap = diasExercicios.reduce((acc, { id_dia, dia_nome, id_exercicio, exercicio_nome, repeticoes, series }) => {
+            if (!acc[id_dia]) {
+                acc[id_dia] = { id_dia, dia_nome, exercicios: [] };
+            }
+            acc[id_dia].exercicios.push({ id_exercicio, exercicio_nome, repeticoes, series });
+            return acc;
+        }, {});
 
-        await knex('treino_dia').where(treinoDiaExists).update({
-            id_treino: idTreinoInfo.id_treino,
-            id_dia: id_dia
-        });
+        const diasList = Object.values(diasMap);
 
-        for (const id_exercicio of id_exercicios) {
-            await knex('exercicio_treino').where({id_treino: idTreinoInfo.id_treino, id_exercicio: id_exercicio}).update({
-                id_treino: idTreinoInfo.id_treino,
-                id_exercicio: id_exercicio
-            });
-        }
+        const response = { treino, dias: diasList };
 
-    const exerciciosList = await knex('exercicio')
-    .join('exercicio_treino', 'exercicio.id_exercicio', 'exercicio_treino.id_exercicio')
-    .where('exercicio_treino.id_treino', id_treino)
-    .select('exercicio.*');
-
-
-        const response = { treino: idTreinoInfo, dia: idDia, exercicios: exerciciosList };
-
-        msgJson(201, res, response, true)
+        msgJson(201, res, response, true);
     } catch (error) {
-        console.error(error)
-        msgJson(500, res, 'Erro interno do servidor ao atualizar treino.', false)
+        console.error(error);
+        msgJson(500, res, 'Erro interno do servidor ao atualizar treino.', false);
     }
-}
+};
 
 export const deleteTraining = async(req, res) => {
     const { params: { id: id_treino }} = req
@@ -133,8 +144,7 @@ export const deleteTraining = async(req, res) => {
         trainingInfo.data_criacao = formattedDates.data_criacao;
         trainingInfo.data_atualizacao = formattedDates.data_atualizacao;
 
-        await knex('treino_dia').where({ id_treino }).first().del()
-        await knex('exercicio_treino').where({ id_treino }).first().del()
+        await knex('treino_dia_exercicio').where({ id_treino }).first().del()
 
         await knex('treino').where({ id_treino }).first().del()
 
@@ -145,32 +155,47 @@ export const deleteTraining = async(req, res) => {
     }
 }
 
-export const getTrainingById = async(req, res) => {
-    const { params: { id: id_treino }} = req
+export const getTrainingById = async (req, res) => {
+    const { params: { id: id_treino } } = req;
 
     try {
+        // Verifica se o treino existe
         const trainingInfo = await knex('treino').where({ id_treino }).first().returning('*');
-        if (!trainingInfo || trainingInfo.length === 0 ) return msgJson(404, res, 'Treino não encontrado.')
+        if (!trainingInfo) return msgJson(404, res, 'Treino não encontrado.');
 
-        const formattedDates = formatDates(trainingInfo.data_criacao,trainingInfo.data_atualizacao,null,3);
-        
+        // Formata as datas
+        const formattedDates = formatDates(trainingInfo.data_criacao, trainingInfo.data_atualizacao, null, 3);
         trainingInfo.data_criacao = formattedDates.data_criacao;
         trainingInfo.data_atualizacao = formattedDates.data_atualizacao;
 
-        const diaInfo = await knex('treino_dia').where({ id_treino }).select('id_dia')
-        const exerciciosList = await knex('exercicio')
-    .join('exercicio_treino', 'exercicio.id_exercicio', 'exercicio_treino.id_exercicio')
-    .where('exercicio_treino.id_treino', id_treino)
-    .select('exercicio.*');
+        // Busca os dias e exercícios associados ao treino
+        const diasExercicios = await knex('treino_dia_exercicio')
+            .join('dia', 'treino_dia_exercicio.id_dia', 'dia.id_dia')
+            .join('exercicio', 'treino_dia_exercicio.id_exercicio', 'exercicio.id_exercicio')
+            .where('treino_dia_exercicio.id_treino', id_treino)
+            .select('dia.id_dia', 'dia.nome as dia_nome', 'exercicio.id_exercicio', 'exercicio.nome as exercicio_nome', 'treino_dia_exercicio.repeticoes', 'treino_dia_exercicio.series');
 
-    const response = { treino: trainingInfo, dia: diaInfo, exercicios: exerciciosList };
+        // Agrupa os exercícios por dia
+        const diasMap = diasExercicios.reduce((acc, { id_dia, dia_nome, id_exercicio, exercicio_nome, repeticoes, series }) => {
+            if (!acc[id_dia]) {
+                acc[id_dia] = { id_dia, dia_nome, exercicios: [] };
+            }
+            acc[id_dia].exercicios.push({ id_exercicio, exercicio_nome, repeticoes, series });
+            return acc;
+        }, {});
 
-        msgJson(201, res, response, true)
+        // Constrói a estrutura de dias com exercícios
+        const diasList = Object.values(diasMap);
+
+        // Monta a resposta final
+        const response = { treino: trainingInfo, dias: diasList };
+
+        msgJson(200, res, response, true);
     } catch (error) {
-        console.error(error)
-        msgJson(500, res, 'Erro interno do servidor ao detalhar Treino.', false)
+        console.error(error);
+        msgJson(500, res, 'Erro interno do servidor ao detalhar Treino.', false);
     }
-}
+};
 
 export const getAllTraining = async(req, res) => {
     try {
