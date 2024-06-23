c89426d3be91e1c4ea85a44ba66d0d186e129425
Gutemberg Lima <j.berglima05@gmail.com>
2024-06-23 14:41:20 -0300

    fix: refact trainingController update

--- backend/src/schemas/schema.js
+++ backend/src/schemas/schema.js
@@ -12,13 +12,24 @@ const email = Joi.string().email().max(30).required(),
     tipo = Joi.string().max(100).required(),
     valor = Joi.number().positive().required(),
     quantidade = Joi.number().integer().positive().required(),
-    id_dia = Joi.number().integer().positive().required(),
-    id_exercicios = Joi.array().items(Joi.number().integer().positive()).required(),
     id_administrador = Joi.number().integer().positive().required(),
     id = Joi.number().integer().positive().required(),
     idOpc = Joi.number().integer().positive(),
     id_academia = Joi.number().integer().positive().required(),
     criaObjJoi = (camposJoiObj) => Joi.object().keys(camposJoiObj).required()
+
+    const dias = Joi.array().items(
+        Joi.object({
+          id_dia: Joi.number().integer().positive().valid(1,2,3,4,5,6,7).required(),
+          exercicios: Joi.array().items(
+            Joi.object({
+              id_exercicio: Joi.number().integer().positive().required(),
+              repeticoes: Joi.number().integer().positive().required(),
+              series: Joi.number().integer().positive().required()
+            })
+          ).min(1).required()
+        })
+      )
 
 
 export const s_idCheck = criaObjJoi({id})
@@ -33,7 +44,7 @@ export const s_admSchema = criaObjJoi({ nome, email, senha, cargo, id_academia }
 
 export const s_plan = criaObjJoi({ tipo, valor, id_academia})
 
-export const s_training = criaObjJoi({ nome, descricao, id_administrador, id_dia, id_exercicios })
+export const s_training = criaObjJoi({ nome, descricao, id_administrador, dias })
 
 export const s_exercise = criaObjJoi({ nome, descricao, id_administrador, gif_url })
 
