6eb5e51eadc818ef657ec17c6c7a60647c063b03
Gutemberg Lima <j.berglima05@gmail.com>
2024-06-23 15:23:40 -0300

    fix: refix conflict merge

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,7 @@ dotenv.config()
 
 const app = express(), port = process.env.PORT || 3000
 
-<<<<<<< HEAD:backend/index.js
-
 app.use(cors());
-=======
-app.use(cors())
->>>>>>> develop:backend/src/index.js
 app.use(express.json(), allRoutes);
 
 app.listen(port, () => console.log(`SERVIDOR ON! NA PORTA ${port} `))
