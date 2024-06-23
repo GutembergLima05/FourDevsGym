043c4a5fadb8af4a88858bc02d60ba57a8ce751e
Gutemberg Lima <j.berglima05@gmail.com>
2024-06-23 09:57:17 -0300

    feat: productController implemented

---
+++ backend/src/routes/productRoutes/productRouter.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { validateEntry, validateTokenAndRole } from "../../middleware/middlewares.js";
+import { s_idCheck, s_product } from "../../schemas/schema.js";
+import * as product from "../../controller/product/productController.js"
+
+export const routeProduct = Router();
+
+routeProduct.route('/product')
+.post(validateEntry(s_product, 'body'), product.register)
+.get(product.getAllProduct)
+
+routeProduct.route('/product/:id')
+.all(validateEntry(s_idCheck, 'params'))
+.put(validateEntry(s_product, 'body'), product.update)
+.get(product.getProductById)
+.delete(product.deleteProduct)
