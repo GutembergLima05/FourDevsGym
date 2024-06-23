8beac9f152b5226d61ae331a2f43214576a3250f
Gutemberg Lima <j.berglima05@gmail.com>
2024-06-23 15:08:33 -0300

    feat: implementing require token

--- backend/src/routes/noticeRoutes/noticeRouter.js
+++ backend/src/routes/noticeRoutes/noticeRouter.js
@@ -1,16 +1,18 @@
 import { Router } from "express";
-import { validateEntry } from "../../middleware/middlewares.js";
+import { validateEntry, validateTokenAndRole } from "../../middleware/middlewares.js";
 import { s_notice, s_idCheck  } from "../../schemas/schema.js";
 import * as notice from "../../controller/notice/noticeController.js"
 
 export const routeNotice = Router();
 
 routeNotice.route('/notice')
+.all(validateTokenAndRole('administrador','id_adm'))
 .post(validateEntry(s_notice, 'body'), notice.register)
 .get(notice.getAllNotice)
 
+
 routeNotice.route('/notice/:id')
-.all(validateEntry(s_idCheck, 'params'))
+.all(validateEntry(s_idCheck, 'params'), validateTokenAndRole('administrador','id_adm'))
 .get(notice.getNoticeById)
 .put(validateEntry(s_notice, 'body'), notice.update)
 .delete(notice.deleteNotice)
