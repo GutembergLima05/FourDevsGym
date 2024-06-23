c40da3f7cd659334c2ff1c039d1d9c14c9e3b958
Gutemberg Lima <j.berglima05@gmail.com>
2024-06-23 18:43:28 -0300

    feat: initialize studentController

---
+++ backend/src/routes/studentRoutes/studentRouter.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { validateEntry, validateTokenAndRole } from "../../middleware/middlewares.js";
+import { s_idCheck, s_student } from "../../schemas/schema.js";
+import * as student from "../../controller/student/studentController.js"
+
+export const routeStudent = Router();
+
+routeStudent.route('/student')
+.all(validateTokenAndRole('administrador','id_adm'))
+.post(validateEntry(s_student, 'body'), student.register)
+.get(student.getAllStudent)
+
+routeStudent.route('/student/:id')
+.all(validateEntry(s_idCheck, 'params'), validateTokenAndRole('administrador','id_adm'))
+.put(validateEntry(s_student, 'body'), student.update)
+.get(student.getStudentById)
+.delete(student.deleteStudent)
