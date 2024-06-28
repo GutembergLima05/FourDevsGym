ff863a09f7fdbe1e71eab76d7f4aa21fcd0fb3fe
Gutemberg Lima <j.berglima05@gmail.com>
2024-06-28 11:43:41 -0300

    fix: index file

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const { allRoutes } = require('./src/routes/exportRoute.js');
 const dotenv = require('dotenv');
 const cors = require('cors');
-dotenv.config()
 
-const app = express(), port = process.env.PORT || 3000
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json(), allRoutes);
+app.use(express.json());
+app.use(allRoutes);
 
-app.listen(port, () => console.log(`SERVIDOR ON! NA PORTA ${port} `))
+app.listen(port, () => {
+  console.log(`SERVIDOR ON! NA PORTA ${port}`);
+});
