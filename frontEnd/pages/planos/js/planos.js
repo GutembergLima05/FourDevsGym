d064d21cf16e7ad722ac5d9767de0d658af3e6ef
victortads <vgs8@discente.ifpe.edu.br>
2024-06-30 15:44:09 -0300

    feat: Funcionalidade de editar, excluir e criar planos

--- frontEnd/pages/planos/js/planos.js
+++ frontEnd/pages/planos/js/planos.js
@@ -1,1 +1,23 @@
-showPlans();
+document.addEventListener("DOMContentLoaded", async()=>{
+    await showPlans();
+    
+    document.getElementById("add-button").addEventListener("click", ()=>{
+        sessionStorage.setItem("actionForm", "add");
+    })
+
+    Array.from(document.getElementsByClassName("edit-button")).forEach(element => {
+        element.addEventListener("click", (event)=>{
+            let id_plano = event.target.closest(".plano-formato").getAttribute("id_plano");
+            sessionStorage.setItem("actionForm", "edit");
+            sessionStorage.setItem("id_plano", id_plano);
+        })
+    });
+
+    Array.from(document.getElementsByClassName("fa-x")).forEach(element => {
+        element.addEventListener("click", async (event)=>{
+            let id_plano = event.target.closest(".plano-formato").getAttribute("id_plano");
+            await deletePlan(id_plano);
+            event.target.closest(".plano-formato").remove();
+        })
+    });
+})
