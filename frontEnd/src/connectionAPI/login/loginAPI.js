40333aefe06c9a8a648e755ed8ac283879acd083
Wilkenio <wilkeniopereiraguitarra@gmail.com>
2024-07-08 07:13:25 -0300

    refactor: enter button at login

--- frontEnd/src/connectionAPI/login/loginAPI.js
+++ frontEnd/src/connectionAPI/login/loginAPI.js
@@ -1,56 +1,63 @@
-buttonEnterAdm.addEventListener("click", function () {
-    checkLogin();
-});
+document.addEventListener('DOMContentLoaded', (event) => {
+    const form = document.getElementById('loginForm');
+    const preloaderLogin = document.getElementById('preloaderLogin');
+    const inputEmailLoginAdm = document.getElementById('InputEmailAdm');
+    const inputPasswordLoginAdm = document.getElementById('InputSenhaAdm');
+    const resultDiv = document.getElementById('msg-login');
 
-async function checkLogin() {
-    // Mostra o preloader
-    preloaderLogin.style.display = 'flex';
+    form.addEventListener('submit', function(e) {
+        e.preventDefault(); // Impede o envio padrão do formulário
+        checkLogin();
+    });
 
-    var email = inputEmailLoginAdm.value;
-    var password = inputPasswordLoginAdm.value;
-    
+    async function checkLogin() {
+        // Mostra o preloader
+        preloaderLogin.style.display = 'flex';
 
-    const url = 'https://apigym-fourdevs.vercel.app/adm/login';
-    const dadosParaEnviar = {
-        email: email,
-        senha: password,
-        key:"3f7e9b8d4c1a2e5b7f6a8c3d9e1b7a2c"
-    };
+        var email = inputEmailLoginAdm.value;
+        var password = inputPasswordLoginAdm.value;
 
-    try {
-        const resposta = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(dadosParaEnviar)
-        });
+        const url = 'https://apigym-fourdevs.vercel.app/adm/login';
+        const dadosParaEnviar = {
+            email: email,
+            senha: password,
+            key: "3f7e9b8d4c1a2e5b7f6a8c3d9e1b7a2c"
+        };
 
-        const dadosResposta = await resposta.json();
+        try {
+            const resposta = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(dadosParaEnviar)
+            });
 
-        if (dadosResposta.success === true) {
-            var tokenAdm = dadosResposta.conteudoJson.token;
-            var nomeAdm = dadosResposta.conteudoJson.usuario.nome;
-            var id_Adm = dadosResposta.conteudoJson.usuario.id_adm;
-            var nome_academia = dadosResposta.conteudoJson.usuario.nome_academia;
+            const dadosResposta = await resposta.json();
 
-            // Armazena os dados no localStorage
-            localStorage.setItem('tokenAdm', tokenAdm);
-            localStorage.setItem('nomeAdm', nomeAdm);
-            localStorage.setItem('id_Adm', id_Adm);
-            localStorage.setItem('nome_academia',nome_academia)
-            // Aqui você pode fazer o que for necessário com os dados recebidos
-            window.location.href = '../../pages/dashboard/dashboard.html';
-        }else{
-            resultDiv.innerText = dadosResposta.conteudoJson.message;
+            if (dadosResposta.success === true) {
+                var tokenAdm = dadosResposta.conteudoJson.token;
+                var nomeAdm = dadosResposta.conteudoJson.usuario.nome;
+                var id_Adm = dadosResposta.conteudoJson.usuario.id_adm;
+                var nome_academia = dadosResposta.conteudoJson.usuario.nome_academia;
+
+                // Armazena os dados no localStorage
+                localStorage.setItem('tokenAdm', tokenAdm);
+                localStorage.setItem('nomeAdm', nomeAdm);
+                localStorage.setItem('id_Adm', id_Adm);
+                localStorage.setItem('nome_academia', nome_academia);
+                
+                // Redireciona para o dashboard
+                window.location.href = '../../pages/dashboard/dashboard.html';
+            } else {
+                resultDiv.innerText = dadosResposta.conteudoJson.message;
+            }
+        } catch (error) {
+            console.error('Erro ao fazer requisição:', error);
+            resultDiv.innerText = "Erro ao fazer login";
+        } finally {
+            // Esconde o preloader, independentemente de ter ocorrido um erro ou não
+            preloaderLogin.style.display = 'none';
         }
-    
-
-    } catch (error) {
-        console.error('Erro ao fazer requisição:', error);
-        resultDiv.innerText = "Erro ao fazer login";
-    } finally {
-        // Esconde o preloader, independentemente de ter ocorrido um erro ou não
-        preloaderLogin.style.display = 'none';
     }
-}
+});
